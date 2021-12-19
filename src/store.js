078742df257d6675cd616a838d6f681c98911731
patricia <patriciapavia236@gmail.com>
2021-12-19 13:54:58 +1100

    add fetchCountryReducer to redux store

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { fetchCountryReducer } from './reducers/fetchCountryReducers';
 const reducer = combineReducers({
-	
+	countryLists: fetchCountryReducer,
 });
 
 const middleware = [thunk];
