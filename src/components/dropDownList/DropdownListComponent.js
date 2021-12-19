01407466c333e9c537c475f6c6c3e986411917e6
patricia <patriciapavia236@gmail.com>
2021-12-19 13:52:46 +1100

    crete dropDownList component to render a createDropDown

---
+++ src/components/dropDownList/DropdownListComponent.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCountryAction } from '../../actions/fetchCountryActions';
+import CreateDropDown from '../createDropDown/CreateDropDown';
+import './DropdownListComponent.css';
+const DropdownListComponent = () => {
+	const dispatch = useDispatch();
+	const countryLists = useSelector((state) => state.countryLists);
+	const { loading, error, countries } = countryLists;
+
+	const [value, setValue] = useState(null);
+
+	useEffect(() => {
+		dispatch(fetchCountryAction());
+	}, []);
+
+	return (
+		<div className='flex-container'>
+			{countries ? (
+				<React.Fragment>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+					<div className='item'>
+						<CreateDropDown
+							options={countries}
+							prompt='Select countries'
+							value={value}
+							onChange={(val) => setValue(val)}
+						/>
+					</div>
+				</React.Fragment>
+			) : (
+				'loading'
+			)}
+		</div>
+	);
+};
+
+export default DropdownListComponent;
